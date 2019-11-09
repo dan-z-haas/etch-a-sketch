6dbc270b1f27328a3cb2ab46e195364c9c5f692e
Dan Haas <dan.z.haas@gmail.com>
2019-11-09 01:58:46 -0500

    js finalized, 100% functional

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,7 @@
 //                              ETCH A SKETCH
 
-//          Creating the interactive Grid
 
-//var divArray = document.querySelectorAll(div);
+//          Creating the interactive Grid
 
 const grid = document.getElementById('container');
 var divsPerSide = 4;
@@ -31,39 +30,18 @@ let fillGrid = function (divsPerSide) {
 
 fillGrid(divsPerSide);
 
-//Hover effect
 
-var hoverEffect = function() {
-    for (i=0;i<pixelArray.length;i++) {
-        pixelArray[i].addEventListener("mouseover", (e) => {
-            e.target.style.backgroundColor = "red";        
-            });
-    }
+//          Select all pixels
+
+var updatePixelArray = function() {
+    pixelArray = document.querySelectorAll('.pixel');    
 }
-var pixelArray = document.querySelectorAll('.pixel');
-
-pixelArray = document.querySelectorAll('.pixel');
-hoverEffect();
-
-//Reset and Resize (R and R) Button
-
-var rAndRButton = document.querySelector('button');
-rAndRButton.addEventListener('click', () => {
-    while (grid.hasChildNodes()) {
-        grid.removeChild(grid.firstChild);
-    }
-    divsPerSide = prompt("How many squares per side?", 10);
-    fillGrid(divsPerSide,divsPerSide);
-    pixelArray = document.querySelectorAll('.pixel');
-    updateDivSize();
-    hoverEffect();
-})
 
 //          Resize Divs to fit Grid
 
 const gridDimensions = 512;
 var divDimensions = gridDimensions/divsPerSide;
-
+updatePixelArray();
 var updateDivSize = function() {
     divDimensions = gridDimensions/divsPerSide;
     for (i=0;i<pixelArray.length;i++) {
@@ -72,3 +50,89 @@ var updateDivSize = function() {
         };
     }
 updateDivSize();
+
+
+//          Reset and Resize (R and R) Button
+
+var rAndRButton = document.querySelector('button');
+rAndRButton.addEventListener('click', () => {
+    while (grid.hasChildNodes()) {
+        grid.removeChild(grid.firstChild);
+    }
+    divsPerSide = prompt("How many squares per side?", 10);
+    fillGrid(divsPerSide,divsPerSide);
+    updatePixelArray();
+    updateDivSize();
+    updateHoverEffect();
+})
+
+
+//          Hover effect
+
+var updateHoverEffect = function() {
+    updatePixelArray();
+    for (i=0;i<pixelArray.length;i++) {
+        pixelArray[i].addEventListener("mouseover", (e) => {
+            
+            if (colorButtonState=="random") {
+                randomColor="rgb("+Math.floor(Math.random()*256)+", "
+                    +Math.floor(Math.random()*256)+", "
+                    +Math.floor(Math.random()*256)+")";
+                trailColor=randomColor;
+            } else {
+                trailColor="rgb(0,0,0)";
+            }
+            e.target.style.backgroundColor = trailColor;
+            
+            if (opacityButtonState==true) {
+                if (e.target.style.opacity=="") {
+                    e.target.style.opacity=0.1
+                } else {
+                e.target.style.opacity=(parseFloat(e.target.style.opacity)+0.1)
+                }
+             } else {
+                e.target.style.opacity=""
+             }
+        })        
+    }
+}
+updateHoverEffect();
+
+
+//          Trail Color Button
+
+let trailColor="rgb(0,0,0)";
+var randomColor;
+    randomColor="rgb("+Math.floor(Math.random()*256)+", "
+        +Math.floor(Math.random()*256)+", "
+        +Math.floor(Math.random()*256)+")";
+
+var colorButton = document.querySelector('#color');
+let colorButtonState="black";
+colorButton.addEventListener('click', () => {
+    if (colorButtonState=="black") {
+        trailColor="rgb(0,0,0)";
+        colorButtonState="random";
+        colorButton.innerText = "Black Trail Color"
+    } else {
+        trailColor=randomColor;
+        colorButtonState="black";
+        colorButton.innerText = "Randomize Trail Colors"
+    }
+    updateHoverEffect();
+})
+
+//          Trail Opacity Mode Button
+
+var opacityButton = document.querySelector('#opacity');
+let opacityButtonState=false;
+opacityButton.addEventListener('click', () => {
+    if (opacityButtonState==false) {
+        opacityButtonState=true;
+        opacityButton.innerText = "Trail Opacity Mode On"
+    } else {
+        opacityButtonState=false;
+        opacityButton.innerText = "Trail Opacity Mode Off"
+    }
+    updateHoverEffect();
+})
